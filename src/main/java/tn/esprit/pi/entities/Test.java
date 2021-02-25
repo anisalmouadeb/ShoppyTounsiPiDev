c87b7af8334442c712e5eb5e3dbe7b9cedea76f5
anisalmouadeb <anis.almouadeb@esprit.tn>
2021-02-25 21:35:50 +0100

    test

---
+++ src/main/java/tn/esprit/pi/entities/Test.java
@@ -0,0 +1,13 @@
+package tn.esprit.pi.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Test implements Serializable{
+
+@Id
+private long testId;
+}
