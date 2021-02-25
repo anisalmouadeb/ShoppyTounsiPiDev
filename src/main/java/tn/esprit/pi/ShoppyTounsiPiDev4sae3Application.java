2de4bf3e9b498c67827bd113db42f918427ee5a0
anisalmouadeb <anis.almouadeb@esprit.tn>
2021-02-25 20:38:22 +0100

    first commit

---
+++ src/main/java/tn/esprit/pi/ShoppyTounsiPiDev4sae3Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.pi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppyTounsiPiDev4sae3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppyTounsiPiDev4sae3Application.class, args);
+	}
+
+}
